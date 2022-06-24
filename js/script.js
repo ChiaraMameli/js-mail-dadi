2d6a5746d0aa7b273cccb0a0e62dd2a4564364a1
ChiaraMameli <mameli.chiara@libero.it>
2022-06-24 14:35:48 +0200

    boilerplate

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/*
+Mail
+Crea (inventa) una lista di inidirizzi mail autorizzati.
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa in console un messaggio appropriato sull’esito del controllo. ("Benvenuto" se la mail è nella lista, "Spiacenti" se non lo è);
+*/
+/*
+Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilisci il vincitore, in base a chi fa il punteggio più alto e stampa  in console il risultato (ossia se ha vinto il computer o il player)
+*/
