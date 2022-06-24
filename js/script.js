b1d1b02db710412e4291ef2f9a7c12f66ce2fea3
ChiaraMameli <mameli.chiara@libero.it>
2022-06-24 15:07:34 +0200

    ex dice completed

--- js/script.js
+++ js/script.js
@@ -1,18 +1,32 @@
+// MAIL
 // Creo Array
 const mail = ['pippo@gmail.com', 'ciccio@gmail.com', 'giorgio@gmail.com', 'michele@gmail.com'];
 
-// Chiedo all'utente di inserire una mail
-let mailInput = prompt('Inserisci qui la tua email', 'pippo@gmail.com');
+ // Chiedo all'utente di inserire una mail
+let mailInput = prompt('Inserisci qui la tua email');
 
-// Verifico che la mail inserita sia valida e stampo un messaggio appropriato sull’esito del controllo.
+ // Verifico che la mail inserita sia valida e stampo un messaggio appropriato sull’esito del controllo.
 if(mail.includes(mailInput)) {
-    alert('Benvenuto');
+     alert('Benvenuto');
 } else {
-    alert('Spiacenti');
+     alert('Spiacenti');
 }
 
-/*
-Gioco dei dadi
-Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
-Stabilisci il vincitore, in base a chi fa il punteggio più alto e stampa  in console il risultato (ossia se ha vinto il computer o il player)
-*/
+// GIOCO DEI DADI
+
+// Genero un numero random da 1 a 6 per i due giocatori
+const player = Math.floor(Math.random() * 6) + 1;
+const computer = Math.floor(Math.random() * 6) + 1;
+console.log(player, computer);
+// Determino il vincitore e stampo in console
+
+if(player > computer) {
+    alert('Hai vinto!');
+} else if (player < computer) {
+    alert('Ritenta!');
+} else {
+    alert('Parità!');
+}
+
+
+
